5db4345e257ce120009f7ea55513b8d70bd65bd0
Waqar Ahmad <codewithwaqarahmad@gmail.com>
2024-10-11 00:34:26 +0500

    Fix deployment issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { lexend, manRope } from "@/lib/fonts";
-import ParticlesComponent from "@/components/initiate-app";
 
 export const metadata: Metadata = {
   title: "Manufacs",
